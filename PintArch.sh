076b29b042eb8aea701695817f7af4987785cc2d
Duarte Pedro Pinto <49436188+dpnpinto@users.noreply.github.com>
2024-02-01 21:25:20 -0100

    Create PintArch.sh

---
+++ PintArch.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#
+# PintArch
+# 
+# Script Inicial que lança cada um dos scripts para cada uma das fazes de instalação
+
+# Find the name of the folder the scripts are in
+set -a
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+SCRIPTS_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"/scripts
+CONFIGS_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"/configs
+set +a
+echo -ne "
+-------------------------------------------------------------------------
+  PintArch
+-------------------------------------------------------------------------
+                    Instlação do PintArch automatizada
+-------------------------------------------------------------------------
+                Os scripts estão num diretório chamado PintArch
+"
+    ( bash $SCRIPT_DIR/scripts/startup.sh )|& tee startup.log
+      source $CONFIGS_DIR/setup.conf
+    ( bash $SCRIPT_DIR/scripts/0-preinstall.sh )|& tee 0-preinstall.log
+    ( arch-chroot /mnt $HOME/ArchTitus/scripts/1-setup.sh )|& tee 1-setup.log
+    if [[ ! $DESKTOP_ENV == server ]]; then
+      ( arch-chroot /mnt /usr/bin/runuser -u $USERNAME -- /home/$USERNAME/ArchTitus/scripts/2-user.sh )|& tee 2-user.log
+    fi
+    ( arch-chroot /mnt $HOME/ArchTitus/scripts/3-post-setup.sh )|& tee 3-post-setup.log
+    cp -v *.log /mnt/home/$USERNAME
+
+echo -ne "
+-------------------------------------------------------------------------
+   PintaArch
+-------------------------------------------------------------------------
+                    Intalação do PintaARch Automatizada
+-------------------------------------------------------------------------
+  Cloncluido - Por favor retirar o dispositivo de arranque e reiniciar 
+"
