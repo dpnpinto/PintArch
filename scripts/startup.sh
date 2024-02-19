651223ef19d80dbd426d02d4a0b418da5f1b8a09
Duarte Pedro Pinto <49436188+dpnpinto@users.noreply.github.com>
2024-02-19 22:06:23 -0100

    Update startup.sh

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -255,14 +255,14 @@ esac
 # @description Set user's keyboard mapping. 
 keymap () {
 echo -ne "
-Please select key board layout from this list"
+Seleciona o layout do teclado desta lista:"
 # These are default key maps as presented in official arch repo archinstall
-options=(us by ca cf cz de dk es et fa fi fr gr hu il it lt lv mk nl no pl ro ru sg ua uk)
+options=(us by ca cf cz de dk es et fa fi fr gr hu il it lt lv mk nl no pl pt ro ru sg ua uk)
 
 select_option $? 4 "${options[@]}"
 keymap=${options[$?]}
 
-echo -ne "Your key boards layout: ${keymap} \n"
+echo -ne "O layout do seu teclado é: ${keymap} \n"
 set_option KEYMAP $keymap
 }
 
