d0f8aad34e7d3161bd0b6d6e51b80f087e8fecb6
Duarte Pedro Pinto <49436188+dpnpinto@users.noreply.github.com>
2024-03-04 14:33:21 -0100

    Update PintArch.sh

--- PintArch.sh
+++ PintArch.sh
@@ -71,8 +71,8 @@ echo -ne "
     # set varibles a store in setup.conf
     ( bash $SCRIPT_DIR/scripts/startup.sh )|& tee startup.log
     source $CONFIGS_DIR/setup.conf
-    ( bash $SCRIPT_DIR/scripts/0-preinstall.sh )|& tee 0-preinstall.log
-    (arch-chroot /mnt bash $HOME/PintArch/scripts/1-setup.sh )|& tee 1-setup.log
+#    ( bash $SCRIPT_DIR/scripts/0-preinstall.sh )|& tee 0-preinstall.log
+#    (arch-chroot /mnt bash $HOME/PintArch/scripts/1-setup.sh )|& tee 1-setup.log
 #    if [[ ! $DESKTOP_ENV == server ]]; then
 #      ( arch-chroot /mnt /usr/bin/runuser -u $USERNAME -- /home/$USERNAME/ArchTitus/scripts/2-user.sh )|& tee 2-user.log
 #    fi
