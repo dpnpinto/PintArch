5c12538d8210f5b65e430aeb30f364a582c82816
Duarte Pedro Pinto <49436188+dpnpinto@users.noreply.github.com>
2024-04-24 14:44:22 +0000

    Update 2-user.sh

--- scripts/2-user.sh
+++ scripts/2-user.sh
@@ -89,6 +89,7 @@ if [[ $DESKTOP_ENV == "DWM" ]]; then
   cp -r /home/$USERNAME/PintArch/configs/backimg/*  /home/$USERNAME/.config/backimg
   nitrogen --random --set-zoom-fill --save /home/$USERNAME/.config/backimg
   mkdir /home/$USERNAME/.themes # Create my themes folder
+  mkdir /home/$USERNAME/.icons # Create my icons folder
   #chown -R $USERNAME: /home/$USERNAME/.config
   chmod 755 /home/$USERNAME/.config/scripts/*   
 fi
