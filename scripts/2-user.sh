5b7363d9bd71d50acd4bcfd64ab54ba191b48f8d
Duarte Pedro Pinto <49436188+dpnpinto@users.noreply.github.com>
2024-04-24 15:49:59 +0000

    Update 2-user.sh

--- scripts/2-user.sh
+++ scripts/2-user.sh
@@ -91,7 +91,7 @@ if [[ $DESKTOP_ENV == "DWM" ]]; then
   mkdir /home/$USERNAME/.themes # Create my themes folder
   unzip /home/$USERNAME/PintArch/configs/themes/Material-Black-Blueberry-2.9.9-07.zip -d /home/$USERNAME/.themes
   mkdir /home/$USERNAME/.icons # Create my Icons themes folder
-  unzip /home/$USERNAME/PintArch/configs/themes/Material-Black-Blueberry-Suru_1.9.3.zip -d /home/$USERNAME/.icons
+  tar -xf /home/$USERNAME/PintArch/configs/themes/Flight-Dark-Icons.tar.gz -C /home/$USERNAME/.icons
   #chown -R $USERNAME: /home/$USERNAME/.config
   chmod 755 /home/$USERNAME/.config/scripts/*   
 fi
