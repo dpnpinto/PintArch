951a33498767d645e1c1bbc97f1b82fefefa5ba7
Duarte Pedro Pinto <49436188+dpnpinto@users.noreply.github.com>
2024-05-27 12:12:12 +0000

    Update 2-user.sh

--- scripts/2-user.sh
+++ scripts/2-user.sh
@@ -71,7 +71,7 @@ if [[ $DESKTOP_ENV == "DWM" ]]; then
                     Vamos instalar o DWM e restante software
   -------------------------------------------------------------------------
   "
-  sudo pacman --noconfirm -S xorg xorg-xinit dmenu
+  sudo pacman --noconfirm -S xorg xorg-xinit alsa-utils
   echo "Clone Pinto Stuff"
   git clone https://github.com/dpnpinto/PintoDWM /home/$USERNAME/.config/PintoDWM
   git clone https://github.com/dpnpinto/PintoST /home/$USERNAME/.config/PintoST
