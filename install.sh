00fa35339c7aeff4abf4ce4ca4fae18658d1b661
Duarte Pedro Pinto <49436188+dpnpinto@users.noreply.github.com>
2024-09-25 16:39:47 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,5 @@
 #!/bin/ sh
-{
-pacman -Sy git glibc
-git clone --noconfirm https://github.com/dpnpinto/PintArch
+pacman -Sy --noconfirm git
+git clone https://github.com/dpnpinto/PintArch
 cd $HOME/PintArch
 bash PintArch.sh
-}
