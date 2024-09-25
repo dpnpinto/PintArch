c7ab327a0d690f107bf7ef5d8e37e8a53e5a5e78
Duarte Pedro Pinto <49436188+dpnpinto@users.noreply.github.com>
2024-09-25 12:30:47 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,4 @@
 #!/bin/ sh
-# Pinto Arch Linux (PintArch) Ĩnstall
-# dpnpinto@gmail.com
-# Installing git
 pacman -Sy --noconfirm --needed git glibc
 git clone https://github.com/dpnpinto/PintArch
 cd $HOME/PintArch
