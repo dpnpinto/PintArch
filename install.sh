16624d47e4fd15a41b0ba946fb6ac7be6cb4fcd2
Duarte Pedro Pinto <49436188+dpnpinto@users.noreply.github.com>
2024-09-25 15:49:35 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/ sh
 pacman -Sy git glibc
-url = "<https://github.com/dpnpinto/PintArch>"
+url = "https://github.com/dpnpinto/PintArch"
 git clone --noconfirm url
 cd $HOME/PintArch
 bash PintArch.sh
