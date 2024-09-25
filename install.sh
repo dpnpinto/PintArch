bca9d73609db96fcb380e4f6a7c54ca9d9bd551e
Duarte Pedro Pinto <49436188+dpnpinto@users.noreply.github.com>
2024-09-25 12:18:32 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,15 +2,10 @@
 # Pinto Arch Linux (PintArch) Ĩnstall
 # dpnpinto@gmail.com
 # Installing git
-
 echo "Instalar o git."
 pacman -Sy --noconfirm --needed git glibc
-
 echo "Clonar o projeto PintArch"
-
 git clone https://github.com/dpnpinto/PintArch
-
 echo "Executar o script de Instalação"
-
 cd $HOME/PintArch
 bash PintArch.sh
